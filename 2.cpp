2a523aee3b6aa509a2104c86455d27fb600e12aa
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-09-24 00:44:35 -0300

    update

--- 2.cpp
+++ 2.cpp
@@ -5,10 +5,22 @@ using namespace std;
 
 class MyClass {        // The class
     public:              // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+    MyClass() {     // Constructor
+      cout << "Constructor\n";
+    }
     void myMethod() {  // Method/function defined inside the class
-        cout << "Hello World!";
+        cout << "Method";
     }
 };
+
+// Derived class
+class MyClass2: public MyClass {
+  public:
+    string model = "class ";
+};
+
 
 int main() {
     MyClass myObj;     // Create an object of MyClass
