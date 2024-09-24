47e95fdc636c9185d234d365a6a388eb1794aa87
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-09-23 22:43:30 -0300

    update

--- 2.cpp
+++ 2.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 
 class MyClass {        // The class
-  public:              // Access specifier
+    public:              // Access specifier
     void myMethod() {  // Method/function defined inside the class
-      cout << "Hello World!";
+        cout << "Hello World!";
     }
 };
 
 int main() {
-  MyClass myObj;     // Create an object of MyClass
-  myObj.myMethod();  // Call the method
-  return 0;
+    MyClass myObj;     // Create an object of MyClass
+    myObj.myMethod();  // Call the method
+    return 0;
 }
