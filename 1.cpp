fece52b7953f6e6aba72da7a5a75675bacf2358e
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-09-23 22:29:19 -0300

    update

--- 1.cpp
+++ 1.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 using namespace std;
+#include <string>
 
 int main() {
-    int myNum = 15;
-    string myText = "Hello\n";   
-    bool myBoolean = false;  
+    int myNum = 5;               // 2 or 4 bytes
+    float myFloatNum = 5.99;     // 4 bytes
+    double myDoubleNum = 9.98;   // 8 bytes
+    char myLetter = 'D';         // 1 byte
+    bool myBoolean = true;       // 1 byte
+    string myText = "Hello";  
 
-    cout << myText;
-    cout << myBoolean << "\n";
+    string firstName = "John ";
+    string lastName = "Doe";
+    string fullName = firstName.append(lastName);
+    cout << fullName;
+
+    cout << "Type your full name: ";
+    getline (cin, fullName);
+    cout << "Your name is: " << fullName;
+
+
+
+    cin >> myNum;
     cout << myNum << "\n";
-    cout << "Hello World!\n";
-    cout << "I am learning C++";
-
+    cout << myFloatNum << "\n";
+    cout << myDoubleNum << "\n";
+    cout << myLetter << "\n";
+    cout << myBoolean << "\n";
+    cout << myText << "\n";
 
     return 0;
 }
